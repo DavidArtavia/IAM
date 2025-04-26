1dd61d7d8f857231823037f4bf6138b0ff8edfb4
Danny <dannycantillanoarias@gmail.com>
2025-04-26 13:31:33 -0600

    CAMBIOS

--- APP/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ APP/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bacd65122aebb96750781861867527798318c971")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d66cbd697263c5c223de0f2613cae409e9686b87")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
