d66cbd697263c5c223de0f2613cae409e9686b87
Danny <dannycantillanoarias@gmail.com>
2025-04-26 13:04:25 -0600

    Homologación Ramas

--- APP/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ APP/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63de3b2c0e81c02655f6578dc1053420fccd7a89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bacd65122aebb96750781861867527798318c971")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
