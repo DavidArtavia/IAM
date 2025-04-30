38fea02a6ad67c4883edd89461d927290827e982
Danny <dannycantillanoarias@gmail.com>
2025-04-29 22:35:29 -0600

    DTB02 DTB03 DTB04 (Avance)

---
+++ DTB/02-Schemas.sql
@@ -0,0 +1,11 @@
+USE IAMDB
+GO
+
+CREATE SCHEMA CORE --Esquema Principal (rempplazando a dbo)
+GO
+CREATE SCHEMA SECU --Esquema de seguridad (para temas de mayor provacidad de datos)
+GO
+CREATE SCHEMA UTIL --Esquema de utilidades para tablas no extrictamente relacionales
+GO
+CREATE SCHEMA REPO --Esquema de para vistas y reportería (historicos, reportes, ETLs, LOGs)
+GO
