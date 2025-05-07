6280fadce92bf4b45825b31515593a013c53efec
Danny <dannycantillanoarias@gmail.com>
2025-05-06 22:55:15 -0600

    Adelanto del flujo de registro de usuarios

---
+++ APP/API/Controllers/UsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        // GET: api/<UsuarioController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+        //[AllowAnonymous]
+        //[EnableCors("Todos")]
+        [Produces("application/json")]
+        [Route("registrarUsuario")]
+        [HttpPost]
+        public void Post([FromBody] DTO_Usuario usuario)
+        {
+
+        }
+
+
+    }
+}
---
+++ APP/DTO/DTO_Usuario.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DTO
+{
+    public class DTO_Usuario
+    {
+        #region Atributos
+
+        private int iD_Usuario;
+        private DTO_Estado estado;
+        private String nombreUsuario;
+        private String apellido;
+        private String telefonoUsuario;
+        private String correoUsuario;
+        private String pass;
+
+        #endregion
+
+
+        #region Constructor
+
+        public DTO_Usuario()
+        {
+
+            ID_Usuario = 0;
+            NombreUsuario = String.Empty;
+            Apellido = String.Empty;
+            TelefonoUsuario = String.Empty;
+            CorreoUsuario = String.Empty;
+            CorreoUsuario = String.Empty;
+            Pass = String.Empty;
+            Estado = new DTO_Estado();
+
+        }
+
+        #endregion
+
+        #region Set´s y Get's
+
+        public int ID_Usuario { get => iD_Usuario; set => iD_Usuario = value; }
+        public string NombreUsuario { get => nombreUsuario; set => nombreUsuario = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string TelefonoUsuario { get => telefonoUsuario; set => telefonoUsuario = value; }
+        public string CorreoUsuario { get => correoUsuario; set => correoUsuario = value; }
+        public string Pass { get => pass; set => pass = value; }
+        internal DTO_Estado Estado { get => estado; set => estado = value; }
+
+
+        #endregion
+    }
+}
