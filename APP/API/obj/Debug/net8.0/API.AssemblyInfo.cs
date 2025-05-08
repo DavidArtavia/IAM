dc7c034c756c15a8142bf4f9e015436b45ddbde0
Danny <dannycantillanoarias@gmail.com>
2025-05-07 19:20:10 -0600

    Se añadió la carpeta BIN al IGNORE

--- APP/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ APP/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3518acb3a7467c0a54e5e229cfa97c396246f045")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddf2ce2fb86393b53ba31bff0ff50d2542e4fa08")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
