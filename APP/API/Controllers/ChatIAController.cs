c588e1fac93f1d33875ab5e6eed770a538cc38a1
Danny <dannycantillanoarias@gmail.com>
2025-05-09 22:57:29 -0600

    Avances en el chat IA

---
+++ APP/API/Controllers/ChatIAController.cs
@@ -0,0 +1,42 @@
+﻿using BLL;
+using DAL;
+using DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech;
+using UTL;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class ChatIAController : Controller
+    {
+        
+
+        [AllowAnonymous]
+        [Produces("application/json")]
+        [Route("chatIA")]
+        [HttpPost]
+         public async Task<DTO_Respuesta> chatIA([FromBody] DTO_Usuario usuario)
+        {
+            DTO_Respuesta respuesta = new DTO_Respuesta();
+   
+            try
+            {
+                await BLL_ChatIA.Transcribir();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+            return respuesta;
+        }
+
+  
+
+
+    }
+}
---
+++ APP/BLL/BLL_ChatIA.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System.Configuration;
+
+namespace BLL
+{
+    public class BLL_ChatIA
+    {
+
+        public async static Task Transcribir()
+        {
+            var speechConfig = SpeechConfig.FromSubscription(ConfigurationManager.AppSettings["AzureKey"] ?? "", ConfigurationManager.AppSettings["AzureRegion"] ?? "");
+            speechConfig.SpeechRecognitionLanguage = "es-ES";
+
+            //using var audioConfig = AudioConfig.FromWavFileInput("C:\\Users\\danny\\Downloads\\test.wav");
+            //using var audioConfig = AudioConfig.FromWavFileInput("https://iamhub7185441083.blob.core.windows.net/audios/test.wav");
+            using var audioConfig = AudioConfig.FromWavFileInput("https://iamhub7185441083.blob.core.windows.net/audios/test.wav?sp=r&st=2025-05-10T04:34:54Z&se=2025-05-10T12:34:54Z&sv=2024-11-04&sr=b&sig=O%2B7SlssdacL%2FTE%2FxvwJOfQFWPi5nMdShSQ3FPk%2FtcA0%3D");
+            using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+            Console.WriteLine("Speak into your microphone.");
+            var speechRecognitionResult = await speechRecognizer.RecognizeOnceAsync();
+            OutputSpeechRecognitionResult(speechRecognitionResult);
+        }
+
+        public static void OutputSpeechRecognitionResult(SpeechRecognitionResult speechRecognitionResult)
+        {
+            switch (speechRecognitionResult.Reason)
+            {
+                case ResultReason.RecognizedSpeech:
+                    Console.WriteLine($"RECOGNIZED: Text={speechRecognitionResult.Text}");
+                    break;
+                case ResultReason.NoMatch:
+                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                    break;
+                case ResultReason.Canceled:
+                    var cancellation = CancellationDetails.FromResult(speechRecognitionResult);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you set the speech resource key and region values?");
+                    }
+                    break;
+            }
+        }
+    }
+}
