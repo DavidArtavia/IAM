a2306ac8237cf73bb1d6ad76f753217e64d07e33
ProgDAVAA <73856384+DavidArtavia@users.noreply.github.com>
2025-05-18 20:10:46 -0600

    Actualizar RequireAuth .tsx
    se crea la recuperación de última pantalla visitanda por el usuario

--- WEB/web-app/src/utils/RequireAuth .tsx
+++ WEB/web-app/src/utils/RequireAuth .tsx
@@ -1,11 +1,15 @@
 // RequireAuth.tsx
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { ROUTES } from "@/constants/routes";
 import { LoadingModal } from "@/components/Modals/LoadingModal/LoadingModal";
 import { useAuthCheck } from "@/hooks/useAuthCheck";
 
 const RequireAuth = () => {
   const isAuth = useAuthCheck();
+  const { pathname, search } = useLocation();
+
+  const lastPath = pathname + search;
+  localStorage.setItem('lastPath', lastPath); 
 
   if (isAuth === null) return <LoadingModal loadingMessage="Verificando..." />;
   return isAuth ? <Outlet /> : <Navigate to={ROUTES.LOGIN} replace />;
