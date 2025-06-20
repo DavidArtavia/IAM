1259c39089165df7fad2f788624c43dffabd733a
Danny <dannycantillanoarias@gmail.com>
2025-06-20 16:10:58 -0600

    Ajuste para recibir nula la descripción del item de la orden de servicio en el bakend

--- APP/DTO/DTO_ItemOrdenServicio.cs
+++ APP/DTO/DTO_ItemOrdenServicio.cs
@@ -10,7 +10,7 @@ namespace DTO
         private int id_OrdenServicio;  // Corresponde al ID de OrdenServicio (FK)
         private DTO_Estado estado;         // Corresponde al ID de Estado (FK)
         private string nombreItemOrdenServicio;
-        private string descripcion;
+        private string? descripcion;
         private decimal monto;
         private int? avance;  // Puede ser NULL, por lo que es de tipo nullable (int?)
 
@@ -37,7 +37,7 @@ namespace DTO
         public int ID_ItemOrdenServicio { get => id_ItemOrdenServicio; set => id_ItemOrdenServicio = value; }
         public int ID_OrdenServicio { get => id_OrdenServicio; set => id_OrdenServicio = value; }
         public string NombreItemOrdenServicio { get => nombreItemOrdenServicio; set => nombreItemOrdenServicio = value; }
-        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public string? Descripcion { get => descripcion; set => descripcion = value; }
         public decimal Monto { get => monto; set => monto = value; }
         public int? Avance { get => avance; set => avance = value; }
         public DTO_Estado Estado { get => estado; set => estado = value; }
