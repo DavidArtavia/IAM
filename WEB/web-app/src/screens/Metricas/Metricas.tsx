5db19f59fcb8f3836bbd047b1f4466057130fd27
Danny <dannycantillanoarias@gmail.com>
2025-07-04 16:41:27 -0600

    ajustes visuales en la pantalla de métricas

--- WEB/web-app/src/screens/Metricas/Metricas.tsx
+++ WEB/web-app/src/screens/Metricas/Metricas.tsx
@@ -23,49 +23,126 @@ export const Metricas = () => {
                         <div className="card-p mt-n20 position-relative">
 
                             <div className="row mt-15">
+
+                                <div className="col bg-light-info px-6 py-8 rounded-2 me-7 mb-7">
+
+
+                                    <span className="svg-icon svg-icon-3x svg-icon-warning d-block my-2">
+                                        <i className="bi bi-arrow-down text-info" style={{ fontSize: 'xx-large' }}></i>
+                                    </span>
+
+                                    <a href="#" className="text-info text-nowrap">
+                                        <span className="fw-bold fs-6">Cuentas&nbsp;Por&nbsp;</span>
+                                        <span className="fw-bolder fs-2">Cobrar</span>
+                                    </a>
+                                    <div className="fw-bolder fs-1 text-info">₡15,300</div>
+                                </div>
                                 <div className="col bg-light-warning px-6 py-8 rounded-2 me-7 mb-7">
 
 
                                     <span className="svg-icon svg-icon-3x svg-icon-warning d-block my-2">
-                                        <i className="bi bi-cash-stack text-warning" style={{ fontSize: 'xx-large' }}></i>
+                                        <i className="bi bi-arrow-up text-warning" style={{ fontSize: 'xx-large' }}></i>
                                     </span>
 
                                     <a href="#" className="text-warning text-nowrap">
                                         <span className="fw-bold fs-6">Cuentas&nbsp;Por&nbsp;</span>
                                         <span className="fw-bolder fs-2">Pagar</span>
-                                    </a><div className="fw-bolder fs-1 text-warning">$1,300</div>
-                                </div><div className="col bg-light-warning px-6 py-8 rounded-2 me-7 mb-7">
-
+                                    </a><div className="fw-bolder fs-1 text-warning">₡1,300</div>
+                                </div>
+                                <div className="col bg-light-primary px-6 py-8 rounded-2 mb-7 me-7">
 
                                     <span className="svg-icon svg-icon-3x svg-icon-warning d-block my-2">
-                                        <i className="bi bi-cash-stack text-warning" style={{ fontSize: 'xx-large' }}></i>
+                                        <i className="bi bi-calculator text-primary" style={{ fontSize: 'xx-large' }}></i>
                                     </span>
-                                    <a href="#" className="text-warning text-nowrap">
+
+
+                                    <a href="#" className="text-primary text-nowrap">
+                                        <span className="fw-bolder fs-2">Balance </span>
+
+                                        <span className="fw-bold fs-6">(CxC-CxP)</span>
+
+                                    </a>
+                                    <div className="fw-bolder fs-1 text-primary">₡14,000</div>
+                                </div>
+                                <div className="col bg-light-success px-6 py-8 rounded-2 mb-7 me-7">
+
+                                    <span className="svg-icon svg-icon-3x svg-icon-warning d-block my-2">
+                                        <i className="bi bi-clipboard-data text-success" style={{ fontSize: 'xx-large' }}></i>
+                                    </span>
+
+
+                                    <a href="#" className="text-success text-nowrap">
+                                        <span className="fw-bolder fs-2">Crecimiento </span>
+
+                                        <span className="fw-bold fs-6">(Comparado con período anterior)</span>
+
+                                    </a>
+                                    <div className="fw-bolder fs-1 text-success">2.5% <span className="fw-bold fs-6">Objetivo: 4%</span></div>
+                                </div>
+
+                            </div>
+
+                            <div className="row g-0">
+
+                            </div>
+
+                        </div>
+
+                        <div className="card-p mt-n20 position-relative">
+
+                            <div className="row mt-15">
+
+                                <div className="col bg-secondary px-6 py-8 rounded-2 me-7 mb-7">
+
+
+                                    <span className="svg-icon svg-icon-3x svg-icon-gray-800 d-block my-2">
+                                        <i className="bi bi-cash-stack text-gray-800" style={{ fontSize: 'xx-large' }}></i>
+                                    </span>
+
+                                    <a href="#" className="text-gray-800 text-nowrap">
                                         <span className="fw-bold fs-6">Cuentas&nbsp;Por&nbsp;</span>
                                         <span className="fw-bolder fs-2">Cobrar</span>
                                     </a>
-                                    <div className="fw-bolder fs-1 text-warning">$15,300</div>
-                                </div><div className="col bg-light-primary px-6 py-8 rounded-2 mb-7 me-7">
-
-                                    <span className="svg-icon svg-icon-3x svg-icon-primary d-block my-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                            <path opacity="0.3" d="M20 15H4C2.9 15 2 14.1 2 13V7C2 6.4 2.4 6 3 6H21C21.6 6 22 6.4 22 7V13C22 14.1 21.1 15 20 15ZM13 12H11C10.5 12 10 12.4 10 13V16C10 16.5 10.4 17 11 17H13C13.6 17 14 16.6 14 16V13C14 12.4 13.6 12 13 12Z" fill="black"></path>
-                                            <path d="M14 6V5H10V6H8V5C8 3.9 8.9 3 10 3H14C15.1 3 16 3.9 16 5V6H14ZM20 15H14V16C14 16.6 13.5 17 13 17H11C10.5 17 10 16.6 10 16V15H4C3.6 15 3.3 14.9 3 14.7V18C3 19.1 3.9 20 5 20H19C20.1 20 21 19.1 21 18V14.7C20.7 14.9 20.4 15 20 15Z" fill="black"></path>
-                                        </svg>
-                                    </span>
-
-                                    <a href="#" className="text-primary fw-bold fs-6">New Projects</a>
-                                </div><div className="col bg-light-success px-6 py-8 rounded-2 mb-7">
-
-                                    <span className="svg-icon svg-icon-3x svg-icon-success d-block my-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                            <path d="M6 8.725C6 8.125 6.4 7.725 7 7.725H14L18 11.725V12.925L22 9.725L12.6 2.225C12.2 1.925 11.7 1.925 11.4 2.225L2 9.725L6 12.925V8.725Z" fill="black"></path>
-                                            <path opacity="0.3" d="M22 9.72498V20.725C22 21.325 21.6 21.725 21 21.725H3C2.4 21.725 2 21.325 2 20.725V9.72498L11.4 17.225C11.8 17.525 12.3 17.525 12.6 17.225L22 9.72498ZM15 11.725H18L14 7.72498V10.725C14 11.325 14.4 11.725 15 11.725Z" fill="black"></path>
-                                        </svg>
-                                    </span>
-
-                                    <a href="#" className="text-success fw-bold fs-6 mt-2">Bug Reports</a>
+                                    <div className="fw-bolder fs-1 text-gray-800">₡15,300</div>
                                 </div>
+                                                                <div className="col bg-secondary px-6 py-8 rounded-2 me-7 mb-7">
+
+
+                                    <span className="svg-icon svg-icon-3x svg-icon-gray-800 d-block my-2">
+                                        <i className="bi bi-cash-stack text-gray-800" style={{ fontSize: 'xx-large' }}></i>
+                                    </span>
+
+                                    <a href="#" className="text-gray-800 text-nowrap">
+                                        <span className="fw-bold fs-6">Cuentas&nbsp;Por&nbsp;</span>
+                                        <span className="fw-bolder fs-2">Cobrar</span>
+                                    </a>
+                                    <div className="fw-bolder fs-1 text-gray-800">₡15,300</div>
+                                </div>                                <div className="col bg-secondary px-6 py-8 rounded-2 me-7 mb-7">
+
+
+                                    <span className="svg-icon svg-icon-3x svg-icon-gray-800 d-block my-2">
+                                        <i className="bi bi-cash-stack text-gray-800" style={{ fontSize: 'xx-large' }}></i>
+                                    </span>
+
+                                    <a href="#" className="text-gray-800 text-nowrap">
+                                        <span className="fw-bold fs-6">Cuentas&nbsp;Por&nbsp;</span>
+                                        <span className="fw-bolder fs-2">Cobrar</span>
+                                    </a>
+                                    <div className="fw-bolder fs-1 text-gray-800">₡15,300</div>
+                                </div>                                <div className="col bg-secondary px-6 py-8 rounded-2 me-7 mb-7">
+
+
+                                    <span className="svg-icon svg-icon-3x svg-icon-gray-800 d-block my-2">
+                                        <i className="bi bi-cash-stack text-gray-800" style={{ fontSize: 'xx-large' }}></i>
+                                    </span>
+
+                                    <a href="#" className="text-gray-800 text-nowrap">
+                                        <span className="fw-bold fs-6">Cuentas&nbsp;Por&nbsp;</span>
+                                        <span className="fw-bolder fs-2">Cobrar</span>
+                                    </a>
+                                    <div className="fw-bolder fs-1 text-gray-800">₡15,300</div>
+                                </div>
+
                             </div>
 
                             <div className="row g-0">
